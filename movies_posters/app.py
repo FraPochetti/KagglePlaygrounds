5be7b6825497a9225bac06e4f32a9ea8df7a2c81
Francesco Pochetti <francesco@ip-172-16-202-149.eu-central-1.compute.internal>
2019-02-22 08:56:31 +0200

    saving work

--- movies_posters/app.py
+++ movies_posters/app.py
@@ -1,29 +1,35 @@
 from chalice import Chalice
+from chalice import BadRequestError
+import base64, os, boto3, ast
+#import numpy as np
 
 app = Chalice(app_name='movies_posters')
+app.debug=True
+endpoint = 'is-thriller-movie-ep--2019-02-21-18-30-19'
 
-
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-    return {'hello': 'world'}
+    body = app.current_request.json_body
 
+    if 'data' not in body:
+        raise BadRequestError('Missing image data')
+    #if 'ENDPOINT_NAME' not in os.environ:
+    #    raise BadRequestError('Missing endpoint')
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    image = base64.b64decode(body['data']) # byte array
+    #endpoint = os.environ['ENDPOINT_NAME'] 
+
+    print(endpoint) 
+
+    runtime = boto3.Session().client(service_name='sagemaker-runtime', region_name='eu-west-1')
+    response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/x-image', Body=image)
+    probs = response['Body'].read().decode() # byte array
+
+    probs = ast.literal_eval(probs) # array of floats
+    pred = probs.index(max(probs))
+    #probs = np.array(probs) # numpy array of floats
+
+    #pred = probs.argmax() 
+    #label = ['Other', 'Thriller/Crime'][pred]
+
+    return {'response': str(probs)}
