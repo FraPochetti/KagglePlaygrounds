5910a474e775183239f532008525de1b830b6eda
Francesco Pochetti <fra.pochetti@gmail.com>
2021-11-03 20:38:49 +0000

    deepfake check

--- deepfake/main.py
+++ deepfake/main.py
@@ -6,18 +6,18 @@ from sagemaker.estimator import Estimator
 
 bucket = 's3://deepfake-fra/'
 
-role = "arn:aws:iam::257446244580:role/sagemaker-icevision"
+role = "myrole"
 
 estimator = Estimator(
     role=role,
-    image_uri="257446244580.dkr.ecr.eu-west-1.amazonaws.com/custom-images:pv",
+    image_uri="myimage",
     instance_count=1,
     instance_type="ml.p3.2xlarge",
     hyperparameters={
         'epochs': 10,
         'lr': 0.1,
     },
-    environment={"WANDB_API_KEY": "aeaed779ca569f4e5336ddddd1d12b234f338d98"},)
+    environment={"WANDB_API_KEY": "mykey"},)
 
 estimator.fit({
     'train': bucket,
